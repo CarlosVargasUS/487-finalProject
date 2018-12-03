aabee5e3afa2e6d277a60c135bc90a8e8eef1f7f
Steveen Vargas <csv17@live.unc.edu>
2018-12-02 22:12:36 -0500

    trying to fix the bug with news.org API

---
+++ js/config.js
@@ -0,0 +1,4 @@
+var config = {
+    MY_KEY : '8e47d5db2c1a43a38db671985863cd61'
+  };
+  
--- js/script.js
+++ js/script.js
@@ -1,40 +1,80 @@
 // YOUTUBE API
 
-var tag = document.createElement('script');
+// var tag = document.createElement('script');
 
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// tag.src = "https://www.youtube.com/iframe_api";
+// var firstScriptTag = document.getElementsByTagName('script')[0];
+// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-// 3. This function creates an <iframe> (and YouTube player)
-//    after the API code downloads.
-var player;
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player('player', {
-    height: '390',
-    width: '640',
-    videoId: 'ygVX1z6tDGI',
-    events: {
-      'onReady': onPlayerReady,
-    }
-  });
+// // 3. This function creates an <iframe> (and YouTube player)
+// //    after the API code downloads.
+// var player;
+// function onYouTubeIframeAPIReady() {
+//   player = new YT.Player('player', {
+//     height: '390',
+//     width: '640',
+//     videoId: 'ygVX1z6tDGI',
+//     events: {
+//       'onReady': onPlayerReady,
+//     }
+//   });
 
-  player.loadVideoById({'videoId': 'bHQqvYy5KYo',
-               'startSeconds': 890,
-               'endSeconds': 999,
-               'suggestedQuality': 'large'});
-}
+//   player.loadVideoById({'videoId': 'bHQqvYy5KYo',
+//                'startSeconds': 890,
+//                'endSeconds': 999,
+//                'suggestedQuality': 'large'});
+// }
 
-// 4. The API will call this function when the video player is ready.
-function onPlayerReady(event) {
-  player.setPlaybackRate(1);
-  event.target.playVideo();
-}
+// // 4. The API will call this function when the video player is ready.
+// function onPlayerReady(event) {
+//   player.setPlaybackRate(1);
+//   event.target.playVideo();
+// }
 
-console.log('I have made it here 4');
+// console.log('I have made it here 4');
+
+  // START OF THE document READY FUNCTION
 
 $(function () {
   console.log("ready!");
+
+
+var myKey = config.MY_KEY;
+var url = 'https://newsapi.org/v2/everything?q=mountain+biking&from=2018-11-14&sortBy=publishedAt&apiKey=' + myKey;
+var url2 = 'https://newsapi.org/v2/everything?q=smurfs&from=2018-11-14&sortBy=publishedAt&apiKey=' + myKey;
+var urlArray = [url, url2];
+var data = [];
+var html = '';
+var articles = [];
+var i = '';
+
+for (i = 0; i < urlArray.length; i ++){
+
+        $.ajax({
+          type:'GET',
+          url: urlArray[i],
+          dataType:'json',
+          async:true,
+          data: data,
+          success:function(data){
+            console.log(data.articles);
+            articles = data.articles;
+
+            articles.forEach(function(article){
+              console.log(article.title);
+              html += '<div class="latest-news flex">';
+                html += '<img class="thumbnail" src="' + article.urlToImage + '">';
+                html += '<div class="text">';
+                html += '<a href="' + article.url + '" target="_blank">';
+                html += '<h2 class="headline">' + article.title + '</h2>';
+                html += '<h4 class="byline">by ' + article.author + ', <em>' + article.source.name + '</em></h4>';
+                html += '</a></div>';
+              html += '</div>';
+            });
+              $('#results').html(html);
+            }
+          });
+}
 
   $(window).scroll(function () {
     parallax();
@@ -104,6 +144,7 @@ $(function () {
   //     }, 10);
   //   });
   // });
+
 
 
 
